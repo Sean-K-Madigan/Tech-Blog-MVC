7a9462ae9c57c65a787c265cc4420146bb75c3c5
Sean-K-Madigan <157637384+Sean-K-Madigan@users.noreply.github.com>
2024-06-02 21:17:30 -0600

    create error.js model

---
+++ models/Error.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Error extends Model {}
+
+Error.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    error_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    error_message: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    error_stack: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    route: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    method: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_agent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'error',
+  }
+);
+
+module.exports = Error;
