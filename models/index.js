4a04960a13f951711350f261ee5711d360d595b9
Sean-K-Madigan <157637384+Sean-K-Madigan@users.noreply.github.com>
2024-06-02 21:23:35 -0600

    create associations in models/index.js

---
+++ models/index.js
@@ -0,0 +1,49 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Error = require('./Error');
+
+// A user can have more than one post
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+// A post can only have one user (creator)
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+// A user can have more than one comment
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+// A comment can only have one user (creator)
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+// A post can have more than one comment
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+// A comment can only have one post
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+// A user can have more than one error
+User.hasMany(Error, {
+  foreignKey: 'user_id',
+});
+
+// An instance of an error can only have one associated user
+Error.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Post, Comment, Error };
